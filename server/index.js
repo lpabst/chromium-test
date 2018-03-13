1266eff3e857c0383aad6ada983cf06f8884ba22
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-03-12 20:18:22 -0600

    added puppeteer code

--- server/index.js
+++ server/index.js
@@ -1,34 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 var massive = require('massive');
 var session = require('express-session');
 var config = require('./config.js');
 
-const app = module.exports = express();
+const puppeteer = require('puppeteer');
 
-app.use(bodyParser.json());
-app.use(session({
+const nodeApp = module.exports = express();
+
+nodeApp.use(bodyParser.json());
+nodeApp.use(session({
   secret: config.secret,
     resave: true,
     saveUninitialized: false,
     cookie:{
-      maxAge: (1000*60*60*24*14) //this is 14 days
+      maxAge: (1000*60*60*24*14)
     }
 }))
 
 massive(config.connection)
 .then( db => {
-  app.set('db', db);
-})
+  nodeApp.set('db', db);
+}).catch(err=>{});
 
-app.use(express.static(__dirname + './../build'))
+nodeApp.use(express.static(__dirname + './../build'))
 
 var userController = require("./userController.js");
+
+
+// puppeteer
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.goto('https://www.instagram.com/');
+  
+  await page.waitFor(200);
+  
+  const input = await page.$('._ev9xl input');
+  await input.click();
+  await page.$eval('._ev9xl input', el => el.value = 'lorenpabst@gmail.com');
+  
+  await page.waitFor(200);
+  await page.screenshot({ path: 'screenshots/instagram.png' });
+  
+  console.log('done');
+  browser.close();
+
+  // await page.click('#react-root > section > main > article > div._kbq82 > div:nth-child(2) > p > a');
+}
+
+run();
+
 
 //////////Endpoints for the front end
 
 
 
 
-app.listen(config.port, console.log("you are now connected on " + config.port));
+nodeApp.listen(config.port, console.log("you are now connected on " + config.port));
