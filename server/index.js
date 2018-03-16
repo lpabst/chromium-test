ec793ed638fa97c498a49d8d58e5a0550a7a43b8
Clayton Pabst <claytonpabst@gmail.com>
2018-03-16 01:55:03 -0600

    goes to peters account and follows 10 people. Tested with muliple accounts and they run simintanuously just fine.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 var massive = require('massive');
@@ -120,11 +122,16 @@ const passwordInput = '#react-root > section > main > article > div._kbq82 > div
 const logInBtn = '#react-root > section > main > article > div._kbq82 > div:nth-child(1) > div > form > span > button';
 const profileLink = 'a[href="/'+config.username+'/"]';
 const followingBtn = '#react-root > section > main > article > header > section > ul > li:nth-child(3) > a';
-followingList = '._gs38e ul div';
+const followingList = '._gs38e ul div';
+
+const followersButton = '#react-root > section > main > article > header > section > ul > li:nth-child(2) > a';
+const followersListReady = 'body > div:nth-child(14) > div > div._o0j5z > div > div._gs38e > ul > div > li:nth-child(1) > div > div._mtnzs > span > button';
+const followersList = '._p4iax > li:nth-child(0) >';
+
 async function instagram(email, password, username, profiles, peopleFollowedByScript) {
   let peopleFollowed = 0;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();  
   
   await page.goto('https://www.instagram.com/');
@@ -137,21 +144,48 @@ async function instagram(email, password, username, profiles, peopleFollowedBySc
   await page.type(passwordInput, password);
   await page.click(logInBtn);
   
-  await page.waitForSelector(profileLink);
-  await page.click(profileLink);
+  // await page.waitForSelector(profileLink);
+  await page.waitFor(10000);
+
+  await page.goto('https://www.instagram.com/psercia/');
+  await page.waitForSelector(followersButton);
+
+  await page.click(followersButton);
+
+  await page.waitForSelector(followersListReady)
+
+  for(let i=1; i<10; i++){
+    let indexToFollow = `body > div:nth-child(14) > div > div._o0j5z > div > div._gs38e > ul > div > li:nth-child(${i}) > div > div._mtnzs > span > button`
+
+    await page.waitFor(6000)
   
-  await page.waitForSelector(followingBtn);
-  await page.click(followingBtn);
+    await page.click(indexToFollow);
+  }
+
+
+  await page.waitFor(5000);
+
+  // await page.click(profileLink);
   
-  await page.waitForSelector(followingList);
+  // await page.waitForSelector(followingBtn);
+  // await page.click(followingBtn);
   
-  await followUnfollow(page, profiles, peopleFollowedByScript);
+  // await page.waitForSelector(followingList);
+
+  let r = Math.floor(Math.random()*1000000);
+  // await page.screenshot({ path: `screenshots/instagram${r}.png` });
+  await page.screenshot({ path: `screenshots/instagram001.png` });
+  
+  // await followUnfollow(page, profiles, peopleFollowedByScript);
 
   // the follow unfollow script runs until the subscription runs out or the user stops the script.
   // When that happens, close the puppeteer browser window
   browser.close();
 
 }
+
+// instagram(config.email, config.password, config.username);
+instagram(config.email2, config.password2, config.username2);
 
 
 
