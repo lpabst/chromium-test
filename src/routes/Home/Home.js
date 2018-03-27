79318e3f4f91b23a2fe2cae734c750a298961bcb
Clayton Pabst <claytonpabst@gmail.com>
2018-03-26 18:37:29 -0600

    Landing message is fixed up

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -43,11 +43,12 @@ class Home extends Component {
         <PictureWithMessage>
           {() => (
             <div style={{backgroundImage:"url('https://wallup.net/wp-content/uploads/2016/01/311247-landscape-beach-people-couple.jpg')"}} className="picture-with-message_background-div">
-              <div className="picture-with-message_gradient-first">
+              
+              <div className="picture-with-message_content-wrapper">
                 <div className="picture-with-message_text-left-wrapper">
                   <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder'}}>Spend More Time</h2>
                   <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder'}}>Doing THIS.</h2>
-                  <p className="picture-with-message_get-started">Get Started</p>
+                  <p style={{paddingLeft:'5px'}} className="picture-with-message_get-started">Get Started</p>
                 </div>
               </div>
             </div>
@@ -61,11 +62,31 @@ class Home extends Component {
         <PictureWithMessage>
           {() => (
             <div style={{backgroundImage:"url('https://iso.500px.com/wp-content/uploads/2016/03/pedroquintela.jpg')"}} className="picture-with-message_background-div">
-              <div className="picture-with-message_gradient-first">
+              <div className="picture-with-message_content-wrapper">
                 <div className="picture-with-message_text-right-wrapper">
                   <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder',}}>Spend More Time</h2>
                   <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder'}}>On CONTENT.</h2>
-                  <p style={{textAlign:'right'}} className="picture-with-message_get-started">Get Started</p>
+                  <div style={{position:'relative'}}>
+                    <p style={{paddingRight:'5px', position:'absolute', right:'0'}} className="picture-with-message_get-started">Get Started</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </PictureWithMessage>
+        < PageNameHeader>
+          {() => (
+            <h1>FIND MORE FOCUS</h1>
+          )}
+        </ PageNameHeader >
+        <PictureWithMessage>
+          {() => (
+            <div style={{backgroundImage:"url('http://shabier.com/images/hd-gym-wallpapers/hd-gym-wallpapers-18.jpg')"}} className="picture-with-message_background-div">
+              <div className="picture-with-message_content-wrapper">
+                <div className="picture-with-message_text-left-wrapper">
+                  <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder',}}>Spend More Time</h2>
+                  <h2 style={{color:'white', fontSize:'4em', fontWeight:'Bolder'}}>On YOU.</h2>
+                    <p style={{paddingLeft:'5px'}} className="picture-with-message_get-started">Get Started</p>
                 </div>
               </div>
             </div>
